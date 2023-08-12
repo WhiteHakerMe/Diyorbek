03bdd2f4415b592b2cc9f45d59005afa9dd3217e
WhiteHakerMe <140898334+WhiteHakerMe@users.noreply.github.com>
2023-08-12 17:40:13 +0500

    Revert "Po"

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from .models import HomeTitle, Category, Post_Dizayne, My_Sills, About_Me, Link_me
+from .models import HomeTitle, Category, Post_Dizayne, My_Sills
 # Register your models here.
 admin.site.register(HomeTitle)
 admin.site.register(Category)
 admin.site.register(Post_Dizayne)
 admin.site.register(My_Sills)
-admin.site.register(About_Me)
-admin.site.register(Link_me)
