b1aaef0e20af44f3190f0d1c3244043555a6619c
WhiteHakerMe <140898334+WhiteHakerMe@users.noreply.github.com>
2023-08-12 20:31:47 +0500

    Revert "Po"

--- app/views.py
+++ app/views.py
@@ -2,15 +2,11 @@ from django.shortcuts import render
 from .models import *
 # Create your views here.
 def index(request):
-    My_link = Link_me.objects.all()
-    My_About = About_Me.objects.all() 
     My_Sill_s = My_Sills.objects.all()
     Post_post = Post_Dizayne.objects.all()
     Home_r = HomeTitle.objects.all()
     Category_ss = Category.objects.all()
     ctx = {
-        "My_link":My_link,
-        "My_About":My_About,
         "My_Sill_s": My_Sill_s,
         "Post_post":Post_post,
         "Home_r":Home_r,
